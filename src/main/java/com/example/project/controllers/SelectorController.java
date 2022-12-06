b75983e75db7636621720587fed3b3d4a7c3ed59
aidar <aidarkhalilov4021@mail.ru>
2022-12-06 10:20:42 +0300

    Updated SQL selector

--- src/main/java/com/example/project/controllers/SelectorController.java
+++ src/main/java/com/example/project/controllers/SelectorController.java
@@ -13,35 +13,39 @@ import java.sql.*;
 @Controller
 public class SelectorController {
 
-    public ResultSet getTable(String query) throws SQLException {
+    public String getResultSet(@RequestParam String query) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/webapplication", "postgres", "aidar344567");
         try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            return rs;
-        } catch (SQLException e) {
-            System.out.println(e.getSQLState());
-            return null;
+            String queryStatement = query.substring(0, 6);
+            if (queryStatement.toLowerCase().equals("select")) {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                String result = "<table id=\"table\" class=\"table table-striped table-hover\"><thead><tr>";
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnCount = rsmd.getColumnCount();
+                for (int i = 0; i < columnCount; i++) {
+                    result += "<th>" + rsmd.getColumnLabel(i + 1) + "</th>";
+                }
+                result += "</tr></thead><tbody>";
+                while (rs.next()) {
+                    result += "<tr>";
+                    for (int i = 0; i < columnCount; i++) {
+                        result += "<td>" + rs.getString(i + 1) + "</td>";
+                    }
+                    result += "</tr>";
+                }
+                result += "</tbody></table>";
+                return result;
+            } else {
+                String result = "<p>Number of affected rows: ";
+                PreparedStatement preparedStatement = conn.prepareStatement(query);
+                int affectedRows = preparedStatement.executeUpdate();
+                result += affectedRows + "</p>";
+                return result;
+            } 
+        } catch (Exception e) {
+            throw new SQLException();
         }
-    }
-
-    public String tableToHTML(ResultSet rs) throws Exception {
-        String result = "<table id=\"table\" class=\"table table-striped table-hover\"><thead><tr>";
-        ResultSetMetaData rsmd = rs.getMetaData();
-        int columnCount = rsmd.getColumnCount();
-        for (int i = 0; i < columnCount; i++) {
-            result += "<th>" + rsmd.getColumnLabel(i + 1) + "</th>";
-        }
-        result += "</tr></thead><tbody>";
-        while (rs.next()) {
-            result += "<tr>";
-            for (int i = 0; i < columnCount; i++) {
-                result += "<td>" + rs.getString(i + 1) + "</td>";
-            }
-            result += "</tr>";
-        }
-        result += "</tbody></table>";
-        return result;
     }
 
     @GetMapping("/selectors")
@@ -52,11 +56,17 @@ public class SelectorController {
     @PostMapping("/selectors")
     public String getTable(@RequestParam String query, Model model) {
         try {
-            model.addAttribute("table", tableToHTML(getTable(query)));
-        } catch (SQLException e) {
-            model.addAttribute("sqlError", e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
+            model.addAttribute("table", getResultSet(query));
+        } catch (SQLException ex) {
+            String sqlException = "";
+            for (Throwable e : ex) {
+                if (e instanceof SQLException) {
+                    sqlException += "<p>SQLState: " + ((SQLException)e).getSQLState() + "</p>";
+                    sqlException += "<p>Error Code: " + ((SQLException)e).getErrorCode() + "</p>";
+                    sqlException += "<p>Message: " + e.getMessage() + "</p>";
+                }
+            }
+            model.addAttribute("sqlException", sqlException);
         }
         return "selectors";
     } 
